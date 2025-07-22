4bfc930adc9d0881a3fd7a750db658c5158e00fe
redjackfred <redjackfred@gmail.com>
2025-07-21 22:06:16 -0700

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: "/newsletter-sign-up-with-success-message/",
 	plugins: [react(), tailwindcss()],
 });
