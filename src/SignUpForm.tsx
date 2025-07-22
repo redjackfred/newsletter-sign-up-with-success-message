04a4b3db3a2070355dffd2612790f38f086b5bf8
redjackfred <redjackfred@gmail.com>
2025-07-21 22:02:25 -0700

    finish

--- src/SignUpForm.tsx
+++ src/SignUpForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import IllustrationMobile from "/src/assets/images/illustration-sign-up-mobile.svg";
 import IllustrationTablet from "/src/assets/images/illustration-sign-up-tablet.svg";
+import IllustrationDesktop from "/src/assets/images/illustration-sign-up-desktop.svg";
 import ListIcon from "/src/assets/images/icon-list.svg";
 import * as z from "zod";
 
@@ -45,7 +46,7 @@ export default function SignUpForm({
 
 	return (
 		<form
-			className="novalidate relative min-h-screen w-full gap-10 bg-white px-6 md:min-h-[878px] md:w-[608px] md:rounded-[36px] md:px-10 md:py-[42.86px]"
+			className="novalidate relative min-h-screen w-full gap-10 bg-white px-6 md:min-h-[878px] md:w-[608px] md:rounded-[36px] md:px-10 md:py-[42.86px] xl:flex xl:h-fit xl:min-h-0 xl:w-fit xl:flex-row-reverse xl:items-center xl:gap-16 xl:px-8 xl:py-6"
 			onSubmit={handleSubmit}
 		>
 			<img
@@ -54,10 +55,14 @@ export default function SignUpForm({
 			/>
 			<img
 				src={IllustrationTablet}
-				className="relative hidden h-[358px] w-full rounded-2xl md:block"
+				className="relative hidden h-[358px] w-full rounded-2xl md:block xl:hidden"
 			/>
-			<div className="relative top-[324px] md:top-10">
-				<section className="flex flex-col gap-6">
+			<img
+				src={IllustrationDesktop}
+				className="relative hidden h-[593px] w-[400px] rounded-2xl xl:block"
+			/>
+			<div className="relative top-[324px] md:top-10 xl:top-0 xl:w-[376px]">
+				<section className="flex flex-col gap-6 xl:gap-8">
 					<h1 className="text-preset-1 text-blue-800">Stay updated!</h1>
 					<p className="text-preset-2-regular">
 						Join 60,000+ product managers receiving monthly updates on:
@@ -77,7 +82,7 @@ export default function SignUpForm({
 						</li>
 					</ul>
 				</section>
-				<section className="mt-10 pb-[44px] md:mt-6 md:pb-[42.86px]">
+				<section className="mt-10 pb-[44px] md:mt-6 md:pb-[42.86px] xl:pb-0">
 					<div className="flex items-center justify-between">
 						<label htmlFor="email" className="text-preset-3 text-blue-800">
 							Email address
@@ -94,7 +99,7 @@ export default function SignUpForm({
 					/>
 					<button
 						type="submit"
-						className="text-preset-2 mt-6 w-full rounded-lg bg-blue-800 p-4 text-white"
+						className={`text-preset-2 mt-6 w-full rounded-lg p-4 text-white ${email == "" ? "bg-blue-800" : "button-color"}`}
 					>
 						{isSubmitting
 							? "Subscribing..."
--- src/ThankMessage.tsx
+++ src/ThankMessage.tsx
@@ -8,17 +8,17 @@ export default function ThankMessage({
 	handleClick: () => void;
 }) {
 	return (
-		<div className="flex min-h-screen flex-col bg-white px-6 pt-[149px] pb-[70px]">
-			<div className="flex flex-col gap-8">
+		<div className="flex min-h-screen flex-col bg-white px-6 pt-[149px] pb-[70px] md:min-h-[520px] md:w-[504px] md:rounded-4xl md:px-[64px] md:py-[60px]">
+			<div className="flex flex-col gap-8 md:w-[376px]">
 				<img src={CheckIcon} className="h-16 w-16" alt="Success Icon" />
 				<h1 className="text-preset-1">Thanks for subscribing!</h1>
 				<p className="text-preset-2-regular text-blue-800">
-					A confirmation email has been sent to {email}. Please open it and
-					click the button inside to confirm your subscription
+					A confirmation email has been sent to <b>{email}</b>. Please open it
+					and click the button inside to confirm your subscription
 				</p>
 			</div>
 			<button
-				className="mt-[263px] h-14 rounded-lg bg-blue-800 text-white"
+				className="text-preset-2 mt-[263px] h-14 rounded-lg bg-blue-800 text-white md:mt-[32px]"
 				onClick={handleClick}
 			>
 				Dismiss message
